59c0b6264ac65223ca3d2733c3ea359b136dd1aa
djbedford <beds2004@hotmail.com>
2017-06-23 00:34:56 +0100

    split app and vendor code into separate bundles

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/App.jsx',
+        vendor: ['react', 'react-dom', 'whatwg-fetch'],
+    },
+    output: {
+        path: '/static',
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.bundle.js'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+        ]
+    }
+};
